d2dd5159e9a734df78a4de5f88582fc16193baee
Gabriel <louis-gabriel.delaigue@poe.aelion.fr>
2024-08-28 13:54:46 +0200

    ajout shared.module en lieu et place de main.module

---
+++ alumni-mob/src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LogoutComponent } from '../logout/components/logout.component';
+import { headerInternComponent } from '../tab2/headerIntern/headerIntern.component';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [LogoutComponent, headerInternComponent],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [LogoutComponent, headerInternComponent]
+})
+export class SharedModule { }
