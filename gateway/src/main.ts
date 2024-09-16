1563a6787f9397d23185bef524363e276d5a1128
jacky <jacky.hoton@poe.aelion.fr>
2024-09-16 15:42:07 +0200

    allow localhost login gateway

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -7,8 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
-    origin: '*',
-    //methods: '*',
+    origin: 'http://localhost:8100',
   });
   app.use(cookieParser());
   app.useGlobalPipes(
